abbcb8fd6e859b35f09ce6bc74dac5ee8155a203
geiselrocha <geiselrocha@hotmail.com>
2022-11-24 22:19:41 -0300

    Requisito 8

--- src/Controllers/MotoController.ts
+++ src/Controllers/MotoController.ts
@@ -52,4 +52,15 @@ export default class MotoController {
       this.next(error);
     }
   }
+
+  public async update() {
+    const { id } = this.req.params;
+    const { body } = this.req;
+    try {
+      const updatedMoto = await this.service.update(id, body);
+      return this.res.status(200).json(updatedMoto);
+    } catch (error) {
+      this.next(error);
+    }
+  }
 }
