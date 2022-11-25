5f870cbbe4492e95f76fd527cf7d713f77de26a6
geiselrocha <geiselrocha@hotmail.com>
2022-11-24 22:09:35 -0300

    Requisito 6

---
+++ tests/unit/Services/vehicle.mock.ts
@@ -0,0 +1,43 @@
+const allVehicle = [
+  {
+    id: '634852326b35b59438fbea2f',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.990,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+  {
+    id: '634852326b35b59438fbea31',
+    model: 'Tempra',
+    year: 1995,
+    color: 'Black',
+    buyValue: 39.000,
+    doorsQty: 2,
+    seatsQty: 5,
+  },
+  {
+    id: '634852326bg5459438fbea31',
+    model: 'Golf',
+    year: 2016,
+    color: 'Black',
+    buyValue: 68.000,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+];
+
+const oneVehicle = {
+  id: '63776ded12654d2053a32384',
+  model: 'Palio',
+  year: 2010,
+  color: 'Black',
+  status: true,
+  buyValue: 20.990,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+export { allVehicle, oneVehicle };
--- tests/unit/Services/vehicle.test.ts
+++ tests/unit/Services/vehicle.test.ts
@@ -4,6 +4,7 @@ import sinon from 'sinon';
 import Car from '../../../src/Domains/Car';
 import ICar from '../../../src/Interfaces/ICar';
 import CarService from '../../../src/Services/CarService';
+import { allVehicle, oneVehicle } from './vehicle.mock';
 
 describe('Test Service Car', function () {
   it('should create a car with success', async function () {
@@ -32,5 +33,21 @@ describe('Test Service Car', function () {
     const result = await service.create(carInput);
     expect(result).to.deep.equal(carOutput);
     sinon.restore();
+  });
+
+  it('should return registered all cars', async function () {
+    const cars: Car[] = allVehicle.map((car) => new Car(car));
+    sinon.stub(Model, 'find').resolves(cars);
+    const service = new CarService();
+    const result = await service.findAll();
+    expect(result).to.deep.equal(cars);
+  });
+
+  it('should successfully search for a car by id', async function () {
+    const car: Car = new Car(oneVehicle);
+    sinon.stub(Model, 'findById').resolves(car);
+    const service = new CarService();
+    const result = await service.findById('634852326bg5459438fbea31');
+    expect(result).to.deep.equal(car);
   });
 });
